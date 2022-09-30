3f59834d38a90a01770c6c2ead5981ea665f24ee
student <i.i.f@mail.ru>
2022-10-01 00:41:12 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/DogsAndCatsShelterApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogsAndCatsShelterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogsAndCatsShelterApplication.class, args);
+	}
+
+}
