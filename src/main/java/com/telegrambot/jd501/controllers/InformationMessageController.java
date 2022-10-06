dd1a40ad1be32371bdd15a9709dd40f327105c08
kirillshilov <sarataza@inbox.ru>
2022-10-06 12:11:56 +0300

    add controllers

--- src/main/java/com/telegrambot/jd501/controllers/InformationMessageController.java
+++ src/main/java/com/telegrambot/jd501/controllers/InformationMessageController.java
@@ -1,124 +1,35 @@
 package com.telegrambot.jd501.controllers;
 
+import com.telegrambot.jd501.Services.InformationMessageService;
 import com.telegrambot.jd501.model.InformationMessage;
-import com.telegrambot.jd501.service.InformationMessageService;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
-
-/**
- * class for work with InformationMessage
- * have CRUD operation
- */
 @RestController
 @RequestMapping("/informationMessage")
 public class InformationMessageController {
 
-    private final InformationMessageService informationMessageService;
+    InformationMessageService informationMessageService;
 
     public InformationMessageController(InformationMessageService informationMessageService) {
         this.informationMessageService = informationMessageService;
     }
 
-    /**
-     * get All InformationMessage from DataBase
-     * Use method of user servise {@link InformationMessageService#getAllInformationMessage()} (Collection< InformationMessage >)}
-     *
-     * @return collection of InformationMessage
-     */
-    @ApiResponses({
-            @ApiResponse(
-                    responseCode = "200",
-                    description = "Show all InformationMessage",
-                    content = @Content(
-                            mediaType = MediaType.APPLICATION_JSON_VALUE,
-                            schema = @Schema(implementation = Collection.class)
-                    )
-            )
-    })
     @GetMapping
-    public Collection<InformationMessage> getAllInformationMessage() {
+    public Collection <InformationMessage> getAllInformationMessage () {
         return informationMessageService.getAllInformationMessage();
     }
-
-    /**
-     * add new InformationMessage in DataBase
-     *
-     * @param informationMessage Use method of Servise {@link InformationMessageService#createInformationMessage(InformationMessage)}
-     * @return InformationMessage
-     */
-    @ApiResponses({
-            @ApiResponse(
-                    responseCode = "200",
-                    description = "Create new InformationMessage",
-                    content = @Content(
-                            mediaType = MediaType.APPLICATION_JSON_VALUE,
-                            schema = @Schema(implementation = InformationMessage.class)
-                    )
-            )
-    })
     @PostMapping
-    public ResponseEntity<InformationMessage> createInformationMessage(@RequestBody InformationMessage informationMessage) {
+    public ResponseEntity<InformationMessage> createInformationMessage(@RequestBody InformationMessage informationMessage){
         return ResponseEntity.ok(informationMessageService.createInformationMessage(informationMessage));
     }
-
-    /**
-     * change InformationMessage in DataBase
-     * Use method of Servise {@link InformationMessageService#updateInformationMessage(InformationMessage)}
-     *
-     * @param informationMessage
-     * @return InformationMessage
-     * @throws com.telegrambot.jd501.Exceptions.InformationMessageNotFoundException if InformationMessage with id not found
-     */
-    @ApiResponses({
-            @ApiResponse(
-                    responseCode = "200",
-                    description = "Change InformationMessage By Id",
-                    content = @Content(
-                            mediaType = MediaType.APPLICATION_JSON_VALUE,
-                            schema = @Schema(implementation = InformationMessage.class)
-                    )
-            ),
-            @ApiResponse(
-                    responseCode = "404",
-                    description = "InformationMessage not found"
-            )
-    })
     @PutMapping
-    public ResponseEntity<InformationMessage> updateInformationMessage(@RequestBody InformationMessage informationMessage) {
+    public ResponseEntity <InformationMessage> updateInformationMessage(@RequestBody InformationMessage informationMessage){
         return ResponseEntity.ok(informationMessageService.updateInformationMessage(informationMessage));
     }
-
-    /**
-     * delete InformationMessage from DataBase by id
-     * Use method of Servise {@link InformationMessageService#deleteInformationMessage(Long id)}
-     *
-     * @param id
-     * @return Deleted InformationMessage
-     * @throws com.telegrambot.jd501.Exceptions.InformationMessageNotFoundException if InformationMessage with id not found
-     */
-    @ApiResponses({
-            @ApiResponse(
-                    responseCode = "200",
-                    description = "Delete InformationMessage By Id",
-                    content = @Content(
-                            mediaType = MediaType.APPLICATION_JSON_VALUE,
-                            schema = @Schema(implementation = InformationMessage.class)
-                    )
-            ),
-            @ApiResponse(
-                    responseCode = "404",
-                    description = "InformationMessage not found"
-            )
-    })
     @DeleteMapping("{id}")
-    ResponseEntity<InformationMessage> deleteInformationMessage(@PathVariable Long id) {
+    ResponseEntity <InformationMessage> deleteInformationMessage (@PathVariable Long id){
         return ResponseEntity.ok(informationMessageService.deleteInformationMessage(id));
     }
 }
--- src/main/java/com/telegrambot/jd501/controllers/UserController.java
+++ src/main/java/com/telegrambot/jd501/controllers/UserController.java
@@ -1,122 +1,28 @@
 package com.telegrambot.jd501.controllers;
 import com.telegrambot.jd501.model.User;
-import com.telegrambot.jd501.service.UserService;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.Collection;
 
-/**
- * class for work with User
- * have CRUD operation
- */
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
-    private final UserService userService;
-
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
-
-    /**
-     * get All User from DataBase
-     * Use method of user servise {@link UserService#getAllUser()} (Collection<User>)}
-     *
-     * @return collection of Users
-     */
-    @ApiResponses({
-            @ApiResponse(
-                    responseCode = "200",
-                    description = "Show all User",
-                    content = @Content(
-                            mediaType = MediaType.APPLICATION_JSON_VALUE,
-                            schema = @Schema(implementation = Collection.class)
-                    )
-            )
-    })
     @GetMapping
-    public Collection<User> getAllUsers() {
-        return userService.getAllUser();
+    public Collection<User> getAllUsers () {
+       return userServise.getAllUser();
     }
-
-    /**
-     * add new User in DataBase
-     *
-     * @param user Use method of Servise {@link UserService#createUser(User)}
-     * @return User
-     */
-    @ApiResponses({
-            @ApiResponse(
-                    responseCode = "200",
-                    description = "Create new User",
-                    content = @Content(
-                            mediaType = MediaType.APPLICATION_JSON_VALUE,
-                            schema = @Schema(implementation = User.class)
-                    )
-            )
-    })
     @PostMapping
-    public ResponseEntity<User> createUser(@RequestBody User user) {
-        return ResponseEntity.ok(userService.createUser(user));
+    public ResponseEntity <User> createUser(@RequestBody User user){
+    return ResponseEntity.ok(userService.createUser(user));
     }
-
-    /**
-     * change User in DataBase
-     * Use method of Servise {@link UserService#updateUser(User)}
-     *
-     * @param user
-     * @return User
-     * @throws com.telegrambot.jd501.Exceptions.UserNotFoundException if User with id not found
-     */
-    @ApiResponses({
-            @ApiResponse(
-                    responseCode = "200",
-                    description = "Change User By Id",
-                    content = @Content(
-                            mediaType = MediaType.APPLICATION_JSON_VALUE,
-                            schema = @Schema(implementation = User.class)
-                    )
-            ),
-            @ApiResponse(
-                    responseCode = "404",
-                    description = "User not found"
-            )
-    })
     @PutMapping
-    public ResponseEntity<User> updateUser(@RequestBody User user) {
+    public ResponseEntity <User> updateUser(@RequestBody User user){
         return ResponseEntity.ok(userService.updateUser(user));
     }
-
-    /**
-     * delete User from DataBase by id
-     * Use method of Servise {@link UserService#deleteUser(Long id)}
-     *
-     * @param id
-     * @return Deleted User
-     * @throws com.telegrambot.jd501.Exceptions.UserNotFoundException if User with id not found
-     */
-    @ApiResponses({
-            @ApiResponse(
-                    responseCode = "200",
-                    description = "Delete User By Id",
-                    content = @Content(
-                            mediaType = MediaType.APPLICATION_JSON_VALUE,
-                            schema = @Schema(implementation = User.class)
-                    )
-            ),
-            @ApiResponse(
-                    responseCode = "404",
-                    description = "User not found"
-            )
-    })
-    @DeleteMapping("{id}")
-    ResponseEntity<User> deleteUser(@PathVariable Long id) {
-        return ResponseEntity.ok(userService.deleteUser(id));
+    @DeleteMapping ("{id}") ResponseEntity <User> deleteUser (@PathVariable Long id){
+       return ResponseEntity.ok(userServise.deleteUser(id));
     }
 
 }
