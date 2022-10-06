3d2802defabbf0524b65fd9fdeb0aee93db566d5
kirillshilov <sarataza@inbox.ru>
2022-10-06 12:35:27 +0300

    add swager

--- src/main/java/com/telegrambot/jd501/PetShelterApplication.java
+++ src/main/java/com/telegrambot/jd501/PetShelterApplication.java
@@ -1,9 +1,11 @@
 package com.telegrambot.jd501;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class PetShelterApplication {
 
 	public static void main(String[] args) {
