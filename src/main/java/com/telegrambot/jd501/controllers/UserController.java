254b6eda8cc37b5ec01a4bd010e163db3f813594
kirillshilov <sarataza@inbox.ru>
2022-10-06 13:30:08 +0300

    add comment to endpoint

--- src/main/java/com/telegrambot/jd501/controllers/UserController.java
+++ src/main/java/com/telegrambot/jd501/controllers/UserController.java
@@ -14,18 +14,22 @@ public class UserController {
     public UserController(UserService userService) {
         this.userService = userService;
     }
+    // get All User from DataBase
     @GetMapping
     public Collection<User> getAllUsers () {
        return userService.getAllUser();
     }
+    // add new User in DataBase
     @PostMapping
     public ResponseEntity <User> createUser(@RequestBody User user){
     return ResponseEntity.ok(userService.createUser(user));
     }
+    // change User in DataBase
     @PutMapping
     public ResponseEntity <User> updateUser(@RequestBody User user){
         return ResponseEntity.ok(userService.updateUser(user));
     }
+    // delete User from DataBase
     @DeleteMapping ("{id}") ResponseEntity <User> deleteUser (@PathVariable Long id){
        return ResponseEntity.ok(userService.deleteUser(id));
     }
