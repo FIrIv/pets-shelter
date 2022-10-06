c10a0fc4d1d45102c6026bd7b7d5116b7d27b586
kirillshilov <sarataza@inbox.ru>
2022-10-06 13:36:44 +0300

    change formatting

--- src/main/java/com/telegrambot/jd501/controllers/VolunteerController.java
+++ src/main/java/com/telegrambot/jd501/controllers/VolunteerController.java
@@ -14,24 +14,28 @@ public class VolunteerController {
     public VolunteerController(VolunteerService volunteerService) {
         this.volunteerService = volunteerService;
     }
+
     // get All Volunteer from DataBase
     @GetMapping
-    public Collection<Volunteer> getAllVolunteer () {
+    public Collection<Volunteer> getAllVolunteer() {
         return volunteerService.getAllVolunteer();
     }
+
     // add new Volunteer in DataBase
     @PostMapping
-    public ResponseEntity<Volunteer> createVolunteer(@RequestBody Volunteer volunteer){
+    public ResponseEntity<Volunteer> createVolunteer(@RequestBody Volunteer volunteer) {
         return ResponseEntity.ok(volunteerService.createVolunteer(volunteer));
     }
+
     // change Volunteer in DataBase
     @PutMapping
-    public ResponseEntity <Volunteer> updateVolunteer(@RequestBody Volunteer volunteer){
+    public ResponseEntity<Volunteer> updateVolunteer(@RequestBody Volunteer volunteer) {
         return ResponseEntity.ok(volunteerService.updateVolunteer(volunteer));
     }
+
     // delete Volunteer from DataBase
     @DeleteMapping("{id}")
-    ResponseEntity <Volunteer> deleteVolunteer (@PathVariable Long id){
+    ResponseEntity<Volunteer> deleteVolunteer(@PathVariable Long id) {
         return ResponseEntity.ok(volunteerService.deleteVolunteer(id));
     }
 }
