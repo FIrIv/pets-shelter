dbec4bfa4afe1824ea57d168b0cea7d8cbaca341
nohero_ <serega1552@yandex.ru>
2022-10-06 12:40:05 +0400

    added all models and repository, but app don't work((

---
+++ src/main/java/com/telegrambot/jd501/repository/VolunteerRepository.java
@@ -0,0 +1,11 @@
+package com.telegrambot.jd501.repository;
+
+import com.telegrambot.jd501.model.Volunteer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VolunteerRepository extends JpaRepository<Volunteer,Long> {
+
+
+}
