0c078a766512fed658b04e324391c1c284d14ab7
student <i.i.f@mail.ru>
2022-11-03 01:00:04 +0800

    добавила метод удаления по id в MailingListServie, перенесла все шедулеры в отдельный сервис, тесты шедулеров для тестовой конфигурации (поминутно) сделала.

---
+++ src/test/java/com/telegrambot/jd501/service/BotSheduleServiceTest.java
@@ -0,0 +1,46 @@
+package com.telegrambot.jd501.service;
+
+import org.awaitility.Duration;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+
+import static org.awaitility.Awaitility.await;
+import static org.mockito.Mockito.atLeast;
+import static org.mockito.Mockito.verify;
+
+// it works just with TEST cron-expressions (every minute expression)
+@SpringBootTest
+class BotSheduleServiceTest {
+
+    @SpyBean
+    private BotSheduleService out;
+
+    @Test
+    void whenWaitTwoMinutesThenSendToVolunteerIfSomethingWrongWithReportsCats() {
+        await()
+                .atMost(Duration.TWO_MINUTES)
+                .untilAsserted(() -> verify(out, atLeast(1)).sendToVolunteerIfSomethingWrongWithReportsCats());
+    }
+
+    @Test
+    void whenWaitTwoMinutesThensendToVolunteerIfSomethingWrongWithReportsDogs() {
+        await()
+                .atMost(Duration.TWO_MINUTES)
+                .untilAsserted(() -> verify(out, atLeast(1)).sendToVolunteerIfSomethingWrongWithReportsDogs());
+    }
+
+    @Test
+    void whenWaitTwoMinutesThensendToVolunteerToDecideAboutDogsAdopter() {
+        await()
+                .atMost(Duration.TWO_MINUTES)
+                .untilAsserted(() -> verify(out, atLeast(1)).sendToVolunteerToDecideAboutDogsAdopter());
+    }
+
+    @Test
+    void whenWaitTwoMinutesThensendToVolunteerToDecideAboutCatsAdopter() {
+        await()
+                .atMost(Duration.TWO_MINUTES)
+                .untilAsserted(() -> verify(out, atLeast(1)).sendToVolunteerToDecideAboutCatsAdopter());
+    }
+}
